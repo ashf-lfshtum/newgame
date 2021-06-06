39fac1808564f271e80d20414170ed258b885958
ashf-lfshtum <84655030+ashf-lfshtum@users.noreply.github.com>
2021-06-06 11:08:02 +0500

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,35 +1,53 @@
+from pygame import *
+from random import randint
+
 class GameSprite(sprite.Sprite):
-#конструктор класса
-  def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-      #вызываем конструктор класса (Sprite):
-      sprite.Sprite.__init__(self)
-      #каждый спрайт должен хранить свойство image - изображение
-      self.image = transform.scale(image.load(player_image), (size_x, size_y))
-      self.speed = player_speed
-      #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
-      self.rect = self.image.get_rect()
-      self.rect.x = player_x
-      self.rect.y = player_y
-#метод, отрисовывающий героя на окне
-  def reset(self):
-      window.blit(self.image, (self.rect.x, self.rect.y))
-#класс главного игрока
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, dx, dy):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.dx = dx
+        self.dy = dy
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        wind.blit(self.image, (self.rect.x, self.rect.y))
+
 class Player(GameSprite):
-   #метод для управления спрайтом стрелками клавиатуры
     def update(self):
-        if 
-        
-    
- 
-   
-#класс спрайта-врага 
+        if self.rect.x >= 700 or self.rect.x <= 0:
+            self.rect.x += dx 
+        if self.rect.y <= 0 or self.rect.y >= 500:
+            self.rect.y += dy
 class Enemy(GameSprite):
-  #движение врага
+
     def update(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-    
-            
+        if keys[K_UP] and self.rect.x >= 0:
+            self.rect.x -= self.dy
+        if keys[K_DOWN] and self.rect.x <= 500:
+            self.rect.x += self.dy
+
+dy = 5
+dx = 5
+font.init()
+font1 = font.Font(None, 80)
+win1 = font1.render('PLAYER 1 WIN!', True, (255, 255, 255))
+win2 = font1.render('PLAYER 2 WIN', True, (255, 255, 255))
+win_width = 700
+win_height = 500
+green = (0, 188, 0)
+wind = display.set_mode((win_width, win_height))
+ball = Player('мячик.png', 300, 250, 20, 20, dx, dy)
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if game:
+        wind.fill(green)
+        ball.reset()
+        ball.update()
+    display.update()
